53e850a39317e72bb9b94c6803cf53d7cfb99d5b
Sergey Parshukov <codedby@bugman.me>
2015-12-04 22:57:55 +0300

    Helloworld with Skotty

--- Main.hs
+++ Main.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.Text.IO as T
+-- import qualified Data.Text.IO as T
+import System.Environment
+import Web.Scotty
 import Repetitions (process)
 
 main :: IO ()
-main = T.interact process >> T.putStrLn ""
+-- main = T.interact process >> T.putStrLn ""
+main = do
+  env <- getEnvironment
+  let port = maybe 8080 read $ lookup "PORT" env
+  scotty port $
+    get "/" $ html "Hello World!"
